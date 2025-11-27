f6ee8f86d4e5407b6d3c6a1d1ed197b3e836f01f
Joseph Hines <jthines3@gmail.com>
2025-11-27 00:29:10 -0500

    Add missing tty input

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 
 
 #archinstall --config-url "http://10.0.2.2:8000/archinstall_config.json" < /dev/tty
-archinstall --config-url "https://raw.githubusercontent.com/ephjos/arch/refs/heads/main/archinstall_config.json"
+archinstall --config-url "https://raw.githubusercontent.com/ephjos/arch/refs/heads/main/archinstall_config.json" < /dev/tty
